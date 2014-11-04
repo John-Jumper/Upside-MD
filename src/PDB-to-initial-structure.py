014a6499f76ffc59e7bc70582e37df32f9a25fe1
John Jumper <jumper@uchicago.edu>
2014-11-04 13:27:08 -0600

    Fixed PDB-to-initial-structure

--- src/PDB-to-initial-structure.py
+++ src/PDB-to-initial-structure.py
@@ -5,13 +5,24 @@ import numpy as np
 import sys
 import cPickle
 
-pdb, chain, output_file = sys.argv[1:]
-structure = prody.parsePDB(pdb)
-coords = structure.select('name N CA C and chain %s' % chain).getCoords()
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('pdb', help='input .pdb file')
+parser.add_argument('output', help='output .pkl file')
+parser.add_argument('--chain', default=None, help='Choose only a specific chain')
+args = parser.parse_args()
+
+structure = prody.parsePDB(args.pdb)
+sel_str = 'name N CA C'
+if args.chain is not None: sel_str += ' and chain %s' % args.chain
+coords = structure.select(sel_str).getCoords()
 print len(coords)/3., 'residues found.'
 bond_lengths = np.sqrt(np.sum(np.diff(coords,axis=0)**2,axis=-1))
-assert bond_lengths.max() < 2.   # check for chain continuity
 
-f=open(output_file,'wb')
+if bond_lengths.max() > 2.:
+    print "WARNING: %i separate chains found" % (bond_lengths>2.).sum()
+
+f=open(args.output,'wb')
 cPickle.dump(coords[...,None], f, -1)
 f.close()
