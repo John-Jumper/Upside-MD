026a8e85b256e2ff42ed8d5de21e2f63ef0cc53e
John Jumper <jumper@uchicago.edu>
2014-12-08 03:30:19 -0600

    Bugfix for counting distinct chains

--- src/PDB_to_initial_structure.py
+++ src/PDB_to_initial_structure.py
@@ -32,7 +32,7 @@ print len(coords)/3., 'residues found.'
 bond_lengths = np.sqrt(np.sum(np.diff(coords,axis=0)**2,axis=-1))
 
 if bond_lengths.max() > 2.:
-    print "WARNING: %i separate chains found" % (bond_lengths>2.).sum()
+    print "WARNING: %i separate chains found" % (1+(bond_lengths>2.).sum())
 
 f=open(args.output,'wb')
 cPickle.dump(coords[...,None], f, -1)
