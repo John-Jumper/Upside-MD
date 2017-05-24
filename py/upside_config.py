1. Modified membrane_potential.cpp to use HBond and environment coverage; correct the derivative due to z_scale. 2. Add a new struct RegisterNodeType to deriv_engine.h in order to register a potential node using 3 coord node. 3. Modify membrane potential in upside_config.py with some unrelated white space removal. 4. Modify PDB_to_initial_structure.py to output residue indices of those with wrong CG-list or CD-list.

py/upside_config.py
@@ -1023,7 +1023,7 @@ def write_membrane_potential(
 
     #<----- ----- ----- ----- make energy splines ----- ----- ----- ----->#
     import scipy.interpolate
-    def extrapolated_spline(x0,y0):
+    def extrapolated_spline(x0, y0):
         spline = scipy.interpolate.InterpolatedUnivariateSpline(x0,y0)
         def f(x, spline=spline):
             return np.select(
@@ -1541,7 +1541,6 @@ def main():
                                  args.membrane_thickness,
                                  args.membrane_exclude_residues, 
                                  args.hbond_exclude_residues)
-
 
     if args.contact_energies:
         require_backbone_point = True