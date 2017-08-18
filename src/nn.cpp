1b7eaa7ff3606e4816c7f7c6475d02a77192e44a
John Jumper <jumper@uchicago.edu>
2017-08-18 15:57:38 -0500

    Added backbone featurizer

--- src/nn.cpp
+++ src/nn.cpp
@@ -12,11 +12,78 @@ inline float relu(float x) {
 }
 
 
+struct BackboneFeaturizer : public CoordNode
+{
+    CoordNode& rama;
+    CoordNode& hbond;
+
+    struct Param {
+        int rama_idx;
+        int donor_idx;
+        int acceptor_idx;
+    };
+    vector<Param> params;
+
+    BackboneFeaturizer(hid_t grp, CoordNode& rama_, CoordNode& hbond_):
+        CoordNode(get_dset_size(1, grp, "rama_idx")[0], 6),
+        rama(rama_),
+        hbond(hbond_),
+        params(n_elem)
+    {
+        check_size(grp, "rama_idx", n_elem);
+        check_size(grp, "hbond_idx", n_elem, 2);
+
+        traverse_dset<1,int>(grp, "rama_idx", [&](size_t ne, int x){params[ne].rama_idx=x;});
+
+        // -1 is special code to indicate no hbond donor or acceptor
+        traverse_dset<2,int>(grp, "hbond_idx", [&](size_t ne, size_t is_acceptor, int x){
+                if(is_acceptor) params[ne].acceptor_idx=x;
+                else            params[ne].donor_idx   =x;});
+    }
+
+    virtual void compute_value(ComputeMode mode) override {
+        VecArray ramac = rama.output;
+        VecArray hbondc = hbond.output;
+
+        for(int ne=0; ne<n_elem; ++ne) {
+            auto& p = params[ne];
+
+            float phi = ramac(0,p.rama_idx);
+            float psi = ramac(1,p.rama_idx);
+            float don_hb = p.donor_idx   ==-1 ? 0.f : hbondc(6,p.donor_idx);
+            float acc_hb = p.acceptor_idx==-1 ? 0.f : hbondc(6,p.acceptor_idx);
+
+            output(0,ne) = sin(phi);
+            output(1,ne) = cos(phi);
+            output(2,ne) = sin(psi);
+            output(3,ne) = cos(psi);
+            output(4,ne) = don_hb;
+            output(5,ne) = acc_hb;
+        }
+    }
+
+    virtual void propagate_deriv() override {
+        VecArray rama_s = rama.sens;
+        VecArray hbond_s = hbond.sens;
+
+        for(int ne=0; ne<n_elem; ++ne) {
+            auto& p = params[ne];
+
+            rama_s(0,p.rama_idx) += sens(1,ne)*(-output(0,ne)) + sens(0,ne)*output(1,ne);
+            rama_s(1,p.rama_idx) += sens(3,ne)*(-output(2,ne)) + sens(2,ne)*output(3,ne);
+            if(p.donor_idx   !=-1) hbond_s(6,p.donor_idx   ) += sens(4,ne);
+            if(p.acceptor_idx!=-1) hbond_s(6,p.acceptor_idx) += sens(5,ne);
+        }
+    }
+};
+static RegisterNodeType<BackboneFeaturizer,2> backbone_featurizer_node("backbone_featurizer");
+
+
 struct Conv1D : public CoordNode
 {
     CoordNode& input;
     int n_elem_input;
-    enum ActivationT {ReLU, Identity};
+    enum ActivationT {ReLU, Tanh, Identity};
 
     int conv_width, in_channels, out_channels;
 
@@ -54,7 +121,8 @@ struct Conv1D : public CoordNode
         auto activation_str = read_attribute<vector<string>>(grp, ".", "activation");
         if(activation_str.size() != 1u) throw string("Invalid number of activations");
 
-        if(activation_str[0] == "ReLU") activation = ReLU;
+        if     (activation_str[0] == "ReLU") activation = ReLU;
+        else if(activation_str[0] == "Tanh") activation = Tanh;
         else if(activation_str[0] == "Identity") activation = Identity;
         else throw string("Invalid activation name");
     }
@@ -69,7 +137,7 @@ struct Conv1D : public CoordNode
         for(int nr=0; nr<n_elem_output; ++nr)
             for(int nw=0; nw<conv_width; ++nw)
                 for(int nc=0; nc<in_channels; ++nc)
-                    input_conv_format(nr,nw*in_channels+nc) = inputc(nc,nr);
+                    input_conv_format(nr,nw*in_channels+nc) = inputc(nc,nr+nw);
        
         // Multiply by weights to make matmul_output
         matmul_output.noalias() = input_conv_format * weights;
@@ -88,6 +156,11 @@ struct Conv1D : public CoordNode
                     for(int nc=0; nc<out_channels; ++nc)
                         output(nc,nr) = relu(matmul_output(nr,nc));
                 break;
+            case Tanh:
+                for(int nr=0; nr<n_elem_output; ++nr)
+                    for(int nc=0; nc<out_channels; ++nc)
+                        output(nc,nr) = tanh(matmul_output(nr,nc));
+                break;
         }
     }
 
@@ -105,6 +178,11 @@ struct Conv1D : public CoordNode
                 for(int nr=0; nr<n_elem_output; ++nr)
                     for(int nc=0; nc<out_channels; ++nc)
                         matmul_output(nr,nc) = output(nc,nr)>0.f ? sens(nc,nr) : 0.f;
+                break;
+            case Tanh:
+                for(int nr=0; nr<n_elem_output; ++nr)
+                    for(int nc=0; nc<out_channels; ++nc)
+                        matmul_output(nr,nc) = sens(nc,nr) * (1.f-sqr(output(nc,nr)));
                 break;
         }
 
@@ -151,4 +229,3 @@ struct ScaledSum: public PotentialNode
     }
 };
 static RegisterNodeType<ScaledSum,1> scaled_sum_node("scaled_sum");
-
